1c66cae5e42676b64a574be4d537401c1ea33dd3
KEREM CAN ÇELEPKOLU <kerem00962@gmail.com>
2024-08-03 00:58:58 +0300

    Validation Error

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,5 +1,3 @@
-const { custom } = require('joi')
-const { CustomAPIError } = require('../errors')
 const { StatusCodes } = require('http-status-codes')
 const errorHandlerMiddleware = (err, req, res, next) => {
   let customError = {
@@ -8,8 +6,14 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong,try again later!"
   }
   
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message })
+  // }
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
   }
   if(err.code && err.code === 11000){
     customError.msg = `Duplicate value entered for ${Object.keys(err.keyValue)} field, please choose another value`
--- models/User.js
+++ models/User.js
@@ -5,13 +5,13 @@ const jwt = require("jsonwebtoken");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: ["true", "please provide a name"],
+    required: [true, "please provide a name"],
     minlength: 3,
     maxlength: 50,
   },
   email: {
     type: String,
-    required: ["true", "please provide a name"],
+    required: [true, "please provide a email"],
     match: [
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
       "Please provide valid email",
@@ -20,7 +20,7 @@ const UserSchema = new mongoose.Schema({
   },
   password: {
     type: String,
-    required: ["true", "please provide a password"],
+    required: [true, "please provide a password"],
     minlength: 6,
   },
 }); 
